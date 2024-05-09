a6970ed685a43bc5a99dc4a0e87843184dc4a0a1
Daniela Elena Iordachiusi <Daniela.Iordachiusi@endava.com>
2024-05-09 20:23:17 +0300

    resolve errors

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
@@ -6,7 +7,7 @@ const Header = () => {
     <header className="bg-pink-500">
       <nav>
         <ul>
-          <li><a href="/trips/create">Create a new trip</a></li>
+          <li><Link legacyBehavior href="/trips/create">Create a new trip</Link></li>
         </ul>
       </nav>
     </header>
@@ -14,3 +15,28 @@ const Header = () => {
 };
 
 export default Header;
+
+
+// import React from 'react';
+// import { useHistory } from 'react-router-dom';
+
+// const Header = () => {
+//   const history = useHistory;
+
+//   const redirectToPage = () => {
+//     history.push('/trips/create');
+//   };
+
+//   return (
+//     <header className="bg-pink-500">
+//       <nav>
+//         <ul>
+//           <li><button onClick={redirectToPage} className="text-white bg-green-500 hover:bg-green-600 px-4 py-2 rounded">Create trip</button></li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
