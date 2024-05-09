975d8002469b6fb2a95c8b14d0ed0055c49124dd
Daniela Elena Iordachiusi <Daniela.Iordachiusi@endava.com>
2024-05-09 19:46:18 +0300

    add new button

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+
+  return (
+    <header className="bg-pink-500">
+      <nav>
+        <ul>
+          <li><a href="/trips/create">Create a new trip</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
